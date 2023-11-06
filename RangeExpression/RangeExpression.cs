a227221aa1b06e3502f24908381e8bc7b1819602
권상구 <milennium9@startergames.com>
2023-11-07 00:02:00 +0900

    RangeExpression Utility 추가

---
+++ RangeExpression/RangeExpression.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Starter.Utilities {
+    public class RangeExpression {
+        private struct Range {
+            private int? start { get; }
+            private int? end   { get; }
+
+            public Range(int? start, int? end) {
+                this.start = start;
+                this.end   = end;
+            }
+
+            public bool IsMatch(int value) {
+                return (start == null || value >= start)
+                    && (end == null || value < end);
+            }
+        }
+
+        private readonly List<Range> _ranges = new();
+
+        private Regex _regex = new(
+            @"(?<begin>-?\d+)?\.\.(?<end>-?\d+)?|(?<value>-?\d+)|\*"
+          , RegexOptions.Singleline | RegexOptions.Compiled
+        );
+
+        public RangeExpression(string pattern) {
+            pattern = pattern.Replace(" ", "");
+            var parts = pattern.Split(',');
+            if (parts == null || !parts.Any())
+                throw new ArgumentException("Invalid pattern - " + pattern, nameof(pattern));
+
+            foreach (var part in parts) {
+                var m = _regex.Match(part);
+                if (m.Value != part)
+                    throw new ArgumentException("Invalid pattern - " + pattern, nameof(pattern));
+                if (!m.Success)
+                    throw new ArgumentException("Invalid pattern - " + pattern, nameof(pattern));
+                
+                if (m.Groups["value"].Success) {
+                    var value = int.Parse(m.Groups["value"].Value);
+                    _ranges.Add(new(value, value + 1));
+                } else if (m.Groups["begin"].Success && m.Groups["end"].Success) {
+                    var begin = int.Parse(m.Groups["begin"].Value);
+                    var end   = int.Parse(m.Groups["end"].Value);
+                    _ranges.Add(new(begin, end + 1));
+                } else if (m.Groups["begin"].Success) {
+                    var begin = int.Parse(m.Groups["begin"].Value);
+                    _ranges.Add(new(begin, null));
+                } else if (m.Groups["end"].Success) {
+                    var end = int.Parse(m.Groups["end"].Value);
+                    _ranges.Add(new(null, end + 1));
+                } else if (m.Groups[0].Value == "*") {
+                    _ranges.Add(new(null, null));
+                }
+            }
+        }
+
+        public bool IsMatch(int value) {
+            return _ranges.Any(range => range.IsMatch(value));
+        }
+    }
+}
---
+++ Test/TestRangeExpression.cs
@@ -0,0 +1,57 @@
+using Starter.Utilities;
+
+namespace Test;
+
+public class TestRangeExpression {
+    [SetUp]
+    public void Setup() { }
+
+    private static IEnumerable<TestCaseData> RandomNumbers {
+        get {
+            for (var i = 0; i < 1000; ++i) yield return new(Random.Shared.Next(-1000, 1000));
+        }
+    }
+
+    [Test, TestCaseSource(nameof(RandomNumbers))]
+    public void TestRandomNumberWithinRange(int num) {
+        var rangePattern = new RangeExpression("..3,5..9,10.. 150,-5 ..-1 ,900..");
+        for (var i = 0; i < 100; i++) // Test with 100 random numbers
+        {
+            var isMatch = rangePattern.IsMatch(num);
+            if (num is <= 3
+                    or >= 5 and <= 9
+                    or >= 10 and <= 150
+                    or >= -5 and <= -1
+                    or >= 900)
+                Assert.That(isMatch, Is.True, $"Number {num} is expected to match but didn't.");
+            else
+                Assert.That(isMatch, Is.False, $"Number {num} is not expected to match but did.");
+        }
+    }
+
+    [Test, TestCaseSource(nameof(RandomNumbers))]
+    public void TestAllNumbers(int num) {
+        var rangePattern = new RangeExpression("*");
+        var isMatch      = rangePattern.IsMatch(num);
+        Assert.That(isMatch, Is.True, $"Number {num} is expected to match but didn't.");
+    }
+
+    [Test, TestCaseSource(nameof(RandomNumbers))]
+    public void TestSingleNumber(int num) {
+        var rangePattern = new RangeExpression("1, 13, 17, 29, 46, -1, -13, -17, -29, -46");
+        var isMatch      = rangePattern.IsMatch(num);
+        if (num is 1 or 13 or 17 or 29 or 46 or -1 or -13 or -17 or -29 or -46) {
+            Assert.That(isMatch, Is.True);
+        }
+        else {
+            Assert.That(isMatch, Is.False);
+        }
+    }
+
+    [Test]
+    public void TestInvalidPattern() {
+        Assert.Catch<ArgumentException>(() => new RangeExpression("1..2..3"));
+        Assert.Catch<ArgumentException>(() => new RangeExpression("1....3,4"));
+        Assert.Catch<ArgumentException>(() => new RangeExpression("abc..d"));
+    }
+}
